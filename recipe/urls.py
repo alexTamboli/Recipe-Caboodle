861d8b02529a8396307957c654db131daf8ad83c
alextamboli <alex.tamboli8@gmail.com>
2023-12-03 02:22:38 +0530

    Recipe api list view served, serializers implemented too

--- recipe/urls.py
+++ recipe/urls.py
@@ -5,7 +5,7 @@ from recipe import views
 app_name = 'recipe'
 
 urlpatterns = [
-    # path('', views.RecipeListAPIView.as_view(), name="recipe-list"),
+    path('', views.RecipeListAPIView.as_view(), name="recipe-list"),
     # path('<int:pk>/', views.RecipeAPIView.as_view(), name="recipe-detail"),
     # path('create/', views.RecipeCreateAPIView.as_view(), name="recipe-create"),
     # path('<int:pk>/like/', views.RecipeLikeAPIView.as_view(),
--- recipe/views.py
+++ recipe/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Recipe, RecipeLike
+from .serializers import RecipeLikeSerializer, RecipeSerializer
+# from .permissions import IsAuthorOrReadOnly
+
+class RecipeListAPIView(generics.ListAPIView):
+    """
+    Get: a collection of recipes
+    """
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+    permission_classes = (AllowAny,)
+    filterset_fields = ('category__name', 'author__username')
