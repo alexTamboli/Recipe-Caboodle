5d04e51648d0736d251709fff93d1662f114a18a
alextamboli <alex.tamboli8@gmail.com>
2023-12-03 19:28:09 +0530

    User profile API avatr and bookmarks api view created

--- users/serializers.py
+++ users/serializers.py
@@ -12,7 +12,34 @@ class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ('id', 'username', 'email')
-        
+
+
+class UserRegisterationSerializer(serializers.ModelSerializer):
+    """
+    Serializer class to serialize registeration requests and create a new user.
+    """
+    class Meta:
+        model = CustomUser
+        fields = ('id', 'username', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        return CustomUser.objects.create_user(**validated_data)
+
+
+class UserLoginSerializer(serializers.Serializer):
+    """
+    Serializer class to authenticate users with email and password.
+    """
+    email = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError('Incorrect Credentials')
+
 
 class ProfileSerializer(CustomUserSerializer):
     """
@@ -30,3 +57,26 @@ class ProfileAvatarSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = ('avatar',)
+
+
+class PasswordChangeSerializer(serializers.Serializer):
+    """
+    Serializer class for changing user password
+    """
+    old_password = serializers.CharField(write_only=True, required=True)
+    new_password = serializers.CharField(write_only=True, required=True)
+
+    def validate_old_password(self, value):
+        user = self.context['request'].user
+        if not user.check_password(value):
+            raise serializers.ValidationError("Old password is not correct")
+        return value
+
+    def validate_new_password(self, value):
+        validate_password(value)
+        return value
+
+    def update(self, instance, validated_data):
+        instance.set_password(validated_data['new_password'])
+        instance.save()
+        return instance
--- users/views.py
+++ users/views.py
@@ -11,6 +11,7 @@ from .models import Profile
 from recipe.serializers import RecipeSerializer
 from . import serializers
 
+User = get_user_model()
 
 class UserAPIView(RetrieveUpdateAPIView):
     """
@@ -33,3 +34,47 @@ class UserProfileAPIView(RetrieveUpdateAPIView):
 
     def get_object(self):
         return self.request.user.profile
+    
+    
+class UserAvatarAPIView(RetrieveUpdateAPIView):
+    """
+    Get, Update user avatar
+    """
+    queryset = Profile.objects.all()
+    serializer_class = serializers.ProfileAvatarSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user.profile
+    
+    
+class UserBookmarkAPIView(ListCreateAPIView):
+    """
+    Get, Create, Delete favorite recipe
+    """
+    serializer_class = RecipeSerializer
+    permission_classes = (IsAuthenticated,)
+    profile = Profile.objects.all()
+
+    def get_queryset(self):
+        user = User.objects.get(id=self.kwargs['pk'])
+        user_profile = get_object_or_404(self.profile, user=user)
+        return user_profile.bookmarks.all()
+
+    def post(self, request, pk):
+        user = User.objects.get(id=pk)
+        user_profile = get_object_or_404(self.profile, user=user)
+        recipe = Recipe.objects.get(id=request.data['id'])
+        if user_profile:
+            user_profile.bookmarks.add(recipe)
+            return Response(status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        user = User.objects.get(id=pk)
+        user_profile = get_object_or_404(self.profile, user=user)
+        recipe = Recipe.objects.get(id=request.data['id'])
+        if user_profile:
+            user_profile.bookmarks.remove(recipe)
+            return Response(status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
