518e5cc9eae254cb0576fa5a587a571105a53f6c
alextamboli <alex.tamboli8@gmail.com>
2023-12-04 20:15:17 +0530

    render push 1

--- build.sh
+++ build.sh
@@ -1,6 +1,16 @@
 echo "BUILD START"
+
 pip install poetry
-python manage.py collectstatic --no-input
-python manage.py migrate
 poetry install
+
+echo "DEPENDENCY INSTALL END/n"
+
+echo "COLLECTSTATIC START"
+python manage.py collectstatic --no-input
+echo "COLLECTSTATIC END/n"
+
+echo "MIGRATE DB"
+python manage.py migrate
+echo "MIGRATE DB END"
+
 echo "BUILD END"
