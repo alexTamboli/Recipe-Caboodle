58edfe0c5045f3b5c3c3d0f0d3e4bc465fbb55b7
alextamboli <alex.tamboli8@gmail.com>
2024-01-21 21:38:20 +0530

    access token- for testing -  expiry changed

--- config/settings/base.py
+++ config/settings/base.py
@@ -183,8 +183,8 @@ SIMPLE_JWT = {
     'JTI_CLAIM': 'jti',
 
     'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
-    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
-    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
+    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=1),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(minutes=2),
 }
 
 # Password reset token lifetime
