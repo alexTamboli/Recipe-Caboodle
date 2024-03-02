b683e8b0d65639e04c78d2b4f45b6d4f1efe7c4d
alextamboli <alex.tamboli8@gmail.com>
2024-03-02 18:32:38 +0530

    token times set

--- config/settings/base.py
+++ config/settings/base.py
@@ -157,8 +157,8 @@ SPECTACULAR_SETTINGS = {
 
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
-    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=2),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=2),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'UPDATE_LAST_LOGIN': False,
